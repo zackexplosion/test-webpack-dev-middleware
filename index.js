d65ac6f1cf142cfd1e91393c9eac7b5b5682cfed
Zack <cstony0917@gmail.com>
2020-06-19 15:03:11 +0800

    add test app code

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const compiler = webpack({
     // 'webpack/hot/dev-server',
     // 'webpack-hot-middleware/client',
     'global': `${js_path}/global.js`,
+    'test': `${js_path}/test.js`,
   },
+  mode: 'development',
   output: {
     filename: '[name].js',
     path: __dirname + '/build',
@@ -26,8 +28,19 @@ const app = express();
 
 app.use(
   middleware(compiler, {
+    publicPath: '/assets-mall/javascripts/'
     // webpack-dev-middleware options
   })
 );
+
+app._router.stack.forEach(function(r){
+  if (r.route && r.route.path){
+    console.log(r.route.path)
+  }
+})
+
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/view/index.html')
+})
 
 app.listen(5000, () => console.log('Example app listening on port 5000!'));
